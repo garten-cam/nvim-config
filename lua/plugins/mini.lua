af4869b5c3ab7512d77781622e149b29be8ae254
Camilo Garcia <camilo_7@msn.com>
2026-05-29 07:41:10 +0200

    completion and snippets for latex

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,6 +1,6 @@
 return {
 	{
-		"echasnovski/mini.surround",
+		"nvim-mini/mini.surround",
 		version = "*",
 		config = function()
 			require("mini.surround").setup({
@@ -19,17 +19,55 @@ return {
 		end,
 	},
 	{
-		"echasnovski/mini.icons",
+		"nvim-mini/mini.icons",
 		version = false,
 		config = function()
 			require("mini.icons").setup()
 		end,
 	},
 	{
-		"echasnovski/mini.pairs",
+		"nvim-mini/mini.pairs",
 		version = "*",
 		config = function()
 			require("mini.pairs").setup()
+		end,
+	},
+	{
+		"nvim-mini/mini.snippets",
+		dependencies = {
+			"rafamadriz/friendly-snippets", -- ensures it's in runtimepath before setup()
+		},
+		version = "*",
+		config = function()
+			local latex_patterns = { "latex/**/*.json", "**/latex.json" }
+			local lang_patterns = {
+				tex = latex_patterns,
+				plaintex = latex_patterns,
+				-- Recognize special injected language of markdown tree-sitter parser
+				markdown_inline = { "markdown.json" },
+			}
+			require("mini.snippets").setup({
+				snippets = {
+					require("mini.snippets").gen_loader.from_lang({ lang_patterns = lang_patterns }),
+					require("mini.snippets").gen_loader.from_file( -- your custom ones
+						vim.fn.stdpath("config") .. "/snippets/{lang}.json"
+					),
+				},
+
+				mappings = {
+					expand = "<Tab>", -- expand or jump forward
+					jump_next = "<Tab>",
+					jump_prev = "<S-Tab>",
+					stop = "<C-c>", -- exit snippet session
+				},
+			})
+		end,
+	},
+	{
+		"nvim-mini/mini.completion",
+		version = "*",
+		config = function()
+			require("mini.completion").setup()
 		end,
 	},
 }
